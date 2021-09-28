256d89bc396237cfa7fd1853faf306ddcc3c8adc
fozy81 <fozy81@hotmail.com>
2021-09-28 17:50:21 +0100

    typo

--- R/get-site-info.R
+++ R/get-site-info.R
@@ -11,9 +11,9 @@
 #' @importFrom jsonlite fromJSON
 #' @importFrom dplyr bind_rows
 #' @examples
-site_info <-
-get_site_info(site_id = c("http://environment.data.gov.uk/ecology/site/bio/43378",
-"http://environment.data.gov.uk/ecology/site/fish/10060"))
+# site_info <-
+# get_site_info(site_id = c("http://environment.data.gov.uk/ecology/site/bio/43378",
+# "http://environment.data.gov.uk/ecology/site/fish/10060"))
 get_site_info <- function(site_id = NULL) {
   site <- URLencode(URL = site_id, reserved = T)
   path = paste0("ecology/api/v1/sites/", site)
