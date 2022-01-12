963669f16f2757ded4d8304ef5125b1973e328c6
fozy81 <fozy81@hotmail.com>
2022-01-12 10:47:32 +0000

    feat: get_taxa() lists function

---
+++ R/get-taxa.R
@@ -0,0 +1,16 @@
+#' Get Taxon Info
+#'
+#' To get a full list of all of species.
+#'
+#' @return Data frame
+#' @export
+#' @importFrom httr GET content
+#' @importFrom jsonlite fromJSON
+#' @examples
+#' properties <- get_taxa()
+get_taxa <- function() {
+  path = paste0("ecology/api/v1/taxa?skip=0&take=18000")
+  response <- GET(ead::ead_data, path = path)
+  data <- fromJSON(content(response, "text"), flatten = TRUE)
+  data <- data.frame(data)
+}
